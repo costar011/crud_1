0bbe004c90620e7be101e6ebaa9ae590f1cd2ba8
costar011 <dnaer456@gmail.com>
2021-04-17 16:34:17 +0900

    pug db

--- src/controller/postController.js
+++ src/controller/postController.js
@@ -4,12 +4,11 @@ export const postController = async (req, res) => {
   try {
     const result = await crud1.find({}, {});
 
-    console.log(result);
+    res.render("post", { status: true, list: result });
   } catch (e) {
     console.log(e);
+    res.render("post", { status: false, list: [] });
   }
-
-  res.render("post");
 };
 
 export const postCreateController = (req, res) => {
