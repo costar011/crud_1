ded90caa32354838c4c344a760921b178d9af3e2
costar011 <dnaer456@gmail.com>
2021-05-08 15:22:33 +0900

    db

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import path from "path";
 import connect from "../db";
 import postRouter from "./routers/postRouter";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "/assets")));
 app.use(morgan(`dev`));
 app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 connect();
 
 app.use("/", postRouter);
--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -3,12 +3,18 @@ import {
   postController,
   postCreateController,
   postDetailController,
+  postCreatePostController,
+  updatePost,
+  postDelete,
 } from "../controller/postController";
 
 const postRouter = express.Router();
 
 postRouter.get("/postList", postController);
 postRouter.get("/postCreate", postCreateController);
+postRouter.post("/postCreate", postCreatePostController);
 postRouter.get("/postDetail/:id", postDetailController);
+postRouter.post("/postDetail", updatePost);
+postRouter.post("/postDelete", postDelete);
 
 export default postRouter;
